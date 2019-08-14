213cb5899492e97b2982a66ed3d63e172730c4bc
Natallia Lukyaniuk <nlukyaniuk@cpaglobal.com>
2019-08-14 20:07:18 +0300

    refactor: add implement report form

--- src/app/main/statistics/report-form/report-form.component.ts
+++ src/app/main/statistics/report-form/report-form.component.ts
@@ -2,11 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { FilterDataService } from '../../../services/filter-data.service';
 
 interface RequiredObject {
-  food: number,
-  rent: number,
-  child: number,
-  gym: number,
-  required: number,
+  food: any,
+  rent: any,
+  child: any,
+  gym: any,
+  required: any,
 }
 
 @Component({
@@ -26,31 +26,51 @@ export class ReportFormComponent implements OnInit {
   listIncomes: Array<object>;
 
   total: number;
+
   currentRequired: RequiredObject = {
-    food: 0,
-    rent: 0,
-    child: 0,
-    gym: 0,
-    required: 0,
+    food: '',
+    rent: '',
+    child: '',
+    gym: '',
+    required: 6,
   };
 
   constructor(private filterDataService: FilterDataService ) { }
 
   ngOnInit() {
+
     this.filterDataService.currentMessageListCoasts.subscribe(data => {
       this.listCoasts = data;
-      console.log(data)
-      this.total = data.reduce((acc, cur) => acc + cur.price, 0);
-      this.currentRequired.food = data.filter(obj => obj.type === 'food').reduce((acc, cur) => acc + cur.price, 0);
-      console.log('gg', data.filter(obj => obj.type === 'food').reduce((acc, cur) => acc + cur.price, 0))
-      this.currentRequired.rent = data.filter(obj => obj.type === 'rent').reduce((acc, cur) => acc + cur.price, 0);
-      this.currentRequired.child = data.filter(obj => obj.type === 'child').reduce((acc, cur) => acc + cur.price, 0);
-      this.currentRequired.gym = data.filter(obj => obj.type === 'gym').reduce((acc, cur) => acc + cur.price, 0);
-      this.currentRequired.required = this.currentRequired.food + this.currentRequired.rent + this.currentRequired.child + this.currentRequired.gym;
+      console.log('this.listCoasts', this.listCoasts)
+      const food = data.filter(obj => obj.type === 'food').reduce((acc, cur) => acc + cur.price, 0);
+      const rent = data.filter(obj => obj.type === 'rent').reduce((acc, cur) => acc + cur.price, 0);
+      const child = data.filter(obj => obj.type === 'child').reduce((acc, cur) => acc + cur.price, 0);
+      const gym = data.filter(obj => obj.type === 'gym').reduce((acc, cur) => acc + cur.price, 0);
 
-     console.log(this.total)
+
+      this.currentRequired = {
+        food: food,
+        rent: rent,
+        child: child,
+        gym: gym,
+        required: food + rent + child + gym,
+      };
+
+      console.log(this.currentRequired.child)
+
+    //   console.log(data)
+    //   this.total = data.reduce((acc, cur) => acc + cur.price, 0);
+    //   this.currentRequired.food = data.filter(obj => obj.type === 'food').reduce((acc, cur) => acc + cur.price, 0);
+    //   console.log('gg', data.filter(obj => obj.type === 'child').reduce((acc, cur) => acc + cur.price, 0))
+    //   this.currentRequired.rent = data.filter(obj => obj.type === 'rent').reduce((acc, cur) => acc + cur.price, 0);
+    //   this.currentRequired.child = data.filter(obj => obj.type === 'child').reduce((acc, cur) => acc + cur.price, 0);
+    //   this.currentRequired.gym = data.filter(obj => obj.type === 'gym').reduce((acc, cur) => acc + cur.price, 0);
+    //   this.currentRequired.required = this.currentRequired.food + this.currentRequired.rent + this.currentRequired.child + this.currentRequired.gym;
+
+    //  console.log(this.total)
 
     });
+
     this.filterDataService.currentMessageListIncomes.subscribe(data => this.listIncomes = data);
 
   }
