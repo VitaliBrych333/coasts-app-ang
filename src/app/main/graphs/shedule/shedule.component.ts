540e8dd8ed4cdf046b81dcb4277d933725d7905d
Vitali Brych <epamc@mail.ru>
2019-08-31 03:55:55 +0300

    feat: add getting data for graphs

--- src/app/main/graphs/shedule/shedule.component.ts
+++ src/app/main/graphs/shedule/shedule.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { ChartDataSets, ChartOptions } from 'chart.js';
 import { Color, BaseChartDirective, Label } from 'ng2-charts';
 import * as pluginAnnotations from 'chartjs-plugin-annotation';
+import { FilterDataService } from '../../../services/filter-data.service';
+
 
 @Component({
   selector: 'app-shedule',
@@ -10,14 +12,10 @@ import * as pluginAnnotations from 'chartjs-plugin-annotation';
 })
 export class SheduleComponent implements OnInit {
 
-  public lineChartData: ChartDataSets[] = [
-    { data: [65, 0, 80, 81, 56, 55, 40], label: 'Series A' },
-    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' },
-    { data: [180, 480, 770, 90, 1000, 270, 400], label: 'Series C', yAxisID: 'y-axis-1' }
-  ];
+  public lineChartData: ChartDataSets[];
 
   public lineChartLabels: Label[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-  public lineChartOptions: (ChartOptions & { annotation: any }) = {
+  public lineChartOptions: ChartOptions = {
     responsive: true,
     maintainAspectRatio: false,
     scales: {
@@ -27,35 +25,22 @@ export class SheduleComponent implements OnInit {
         {
           id: 'y-axis-0',
           position: 'left',
-        },
-        {
-          id: 'y-axis-1',
-          position: 'right',
-          gridLines: {
-            color: 'rgba(255,0,0,0.3)',
-          },
           ticks: {
-            fontColor: 'red',
-          }
-        }
-      ]
-    },
-    annotation: {
-      annotations: [
-        {
-          type: 'line',
-          mode: 'vertical',
-          scaleID: 'x-axis-0',
-          value: 'March',
-          borderColor: 'orange',
-          borderWidth: 2,
-          label: {
-            enabled: true,
-            fontColor: 'orange',
-            content: 'LineAnno'
+            beginAtZero: true
           }
         },
-      ],
+        // {
+        //   id: 'y-axis-1',
+        //   position: 'right',
+        //   gridLines: {
+        //     color: 'rgba(255,0,0,0.3)',
+        //   },
+        //   ticks: {
+        //     fontColor: 'red',
+        //     beginAtZero: true
+        //   }
+        // }
+      ]
     },
   };
   public lineChartColors: Color[] = [
@@ -90,55 +75,72 @@ export class SheduleComponent implements OnInit {
 
   @ViewChild(BaseChartDirective, { static: true }) chart: BaseChartDirective;
 
-  constructor() { }
+  constructor(public filterDataService: FilterDataService) { }
 
   ngOnInit() {
+    this.filterDataService.currentDataCompare.subscribe(data => {
 
-  }
-
-  public randomize(): void {
-    for (let i = 0; i < this.lineChartData.length; i++) {
-      for (let j = 0; j < this.lineChartData[i].data.length; j++) {
-        this.lineChartData[i].data[j] = this.generateNumber(i);
+      this.lineChartData = [
+        { data: [], label: '' },
+        { data: [], label: '' },
+        { data: [], label: '' }
+      ];
+// yAxisID: 'y-axis-1'
+      if (data.length) {
+        data.forEach((value, index) => {
+           let newData = Array.from(value.values()).slice(0, 12);
+           let newLabel = value.get(12);
+           this.lineChartData[index].data = newData;
+           this.lineChartData[index].label = newLabel;
+        })
       }
-    }
-    this.chart.update();
+
+    })
   }
 
-  private generateNumber(i: number) {
-    return Math.floor((Math.random() * (i < 2 ? 100 : 1000)) + 1);
-  }
+  // public randomize(): void {
+  //   for (let i = 0; i < this.lineChartData.length; i++) {
+  //     for (let j = 0; j < this.lineChartData[i].data.length; j++) {
+  //       this.lineChartData[i].data[j] = this.generateNumber(i);
+  //     }
+  //   }
+  //   this.chart.update();
+  // }
+
+  // private generateNumber(i: number) {
+  //   return Math.floor((Math.random() * (i < 2 ? 100 : 1000)) + 1);
+  // }
 
   // events
-  public chartClicked({ event, active }: { event: MouseEvent, active: {}[] }): void {
-    console.log(event, active);
-  }
+  // public chartClicked({ event, active }: { event: MouseEvent, active: {}[] }): void {
+  //   console.log(event, active);
+  // }
 
-  public chartHovered({ event, active }: { event: MouseEvent, active: {}[] }): void {
-    console.log(event, active);
-  }
+  // public chartHovered({ event, active }: { event: MouseEvent, active: {}[] }): void {
+  //   console.log(event, active);
+  // }
 
-  public hideOne() {
-    const isHidden = this.chart.isDatasetHidden(1);
-    this.chart.hideDataset(1, !isHidden);
-  }
+  // public hideOne() {
+  //   const isHidden = this.chart.isDatasetHidden(1);
+  //   this.chart.hideDataset(1, !isHidden);
+  // }
 
-  public pushOne() {
-    this.lineChartData.forEach((x, i) => {
-      const num = this.generateNumber(i);
-      const data: number[] = x.data as number[];
-      data.push(num);
-    });
-    this.lineChartLabels.push(`Label ${this.lineChartLabels.length}`);
-  }
+  // public pushOne() {
+  //   this.lineChartData.forEach((x, i) => {
+  //     const num = this.generateNumber(i);
+  //     const data: number[] = x.data as number[];
+  //     data.push(num);
+  //   });
+  //   this.lineChartLabels.push(`Label ${this.lineChartLabels.length}`);
+  // }
 
   public changeColor() {
     this.lineChartColors[2].borderColor = 'green';
     this.lineChartColors[2].backgroundColor = `rgba(0, 255, 0, 0.3)`;
   }
 
-  public changeLabel() {
-    this.lineChartLabels[2] = ['1st Line', '2nd Line'];
-    // this.chart.update();
-  }
+  // public changeLabel() {
+  //   this.lineChartLabels[2] = ['1st Line', '2nd Line'];
+  //   // this.chart.update();
+  // }
 }
