efc22dac3f22d7c158f6af0d9dfcd1f0a1c2a1e1
Vitali Brych <epamc@mail.ru>
2020-02-16 02:34:45 +0300

    refactor: rewrite buttons

--- src/app/main/buttons/buttons.component.ts
+++ src/app/main/buttons/buttons.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Url } from '../../shared/constants/url-enum';
+import { Buttons } from '../../shared/constants/button-enum';
+
+interface Button {
+  title: string,
+  link: string
+}
 
 @Component({
   selector: 'app-buttons',
@@ -10,7 +16,7 @@ import { Url } from '../../shared/constants/url-enum';
 
 export class ButtonsComponent implements OnInit {
 
-  private currentUrl: string = this.router.url;
+  public currentUrl: string = this.router.url;
   private stateOne: boolean = false;
   private stateTwo: boolean = false;
   private stateTree: boolean = false;
@@ -18,53 +24,66 @@ export class ButtonsComponent implements OnInit {
   private stateFive: boolean = false;
   private stateSix: boolean = false;
 
+  public buttons: Button[] = [
+    { title: Buttons.ADDCOAST, link: Url.NEWPURCH },
+    { title: Buttons.ADDINCOME, link: Url.NEWINC },
+    { title: Buttons.SHOWCOASTS, link: Url.ALLPURCH },
+    { title: Buttons.SHOWINCOMES, link: Url.ALLINC },
+    { title: Buttons.STATIST, link: Url.STATIST },
+    { title: Buttons.GRAPHS, link: Url.GRAPHS },
+  ];
+
   constructor(private router: Router) {}
 
   public ngOnInit(): void {
-    switch (this.currentUrl) {
-      case Url.NEWPURCH:
-        this.stateOne = true;
-        break;
-      case Url.NEWINC:
-        this.stateTwo = true;
-        break;
-      case Url.ALLPURCH:
-        this.stateTree = true;
-        break;
-      case Url.ALLINC:
-        this.stateFour = true;
-        break;
-      case Url.STATIST:
-        this.stateFive = true;
-        break;
-      case Url.GRAPHS:
-        this.stateSix = true;
-        break;
-      default:
-    }
+    // switch (this.currentUrl) {
+    //   case Url.NEWPURCH:
+    //     this.stateOne = true;
+    //     break;
+    //   case Url.NEWINC:
+    //     this.stateTwo = true;
+    //     break;
+    //   case Url.ALLPURCH:
+    //     this.stateTree = true;
+    //     break;
+    //   case Url.ALLINC:
+    //     this.stateFour = true;
+    //     break;
+    //   case Url.STATIST:
+    //     this.stateFive = true;
+    //     break;
+    //   case Url.GRAPHS:
+    //     this.stateSix = true;
+    //     break;
+    //   default:
+    // }
   }
 
-  public addCoasts(): void {
-    this.router.navigate([Url.NEWPURCH]);
+  public handleClick(url: string): void {
+    this.router.navigate([url]);
   }
 
-  public addIncome(): void {
-    this.router.navigate([Url.NEWINC]);
-  }
+  // public addCoasts(): void {
+  //   this.router.navigate([Url.NEWPURCH]);
+  // }
 
-  public showTableCoasts(): void {
-    this.router.navigate([Url.ALLPURCH]);
-  }
+  // public addIncome(): void {
+  //   this.router.navigate([Url.NEWINC]);
+  // }
 
-  public showTableIncomes(): void {
-    this.router.navigate([Url.ALLINC]);
-  }
+  // public showTableCoasts(): void {
+  //   this.router.navigate([Url.ALLPURCH]);
+  // }
 
-  public getStatistics(): void {
-    this.router.navigate([Url.STATIST]);
-  }
+  // public showTableIncomes(): void {
+  //   this.router.navigate([Url.ALLINC]);
+  // }
 
-  public showGraphs(): void {
-    this.router.navigate([Url.GRAPHS]);
-  }
+  // public getStatistics(): void {
+  //   this.router.navigate([Url.STATIST]);
+  // }
+
+  // public showGraphs(): void {
+  //   this.router.navigate([Url.GRAPHS]);
+  // }
 }
---
+++ src/app/shared/constants/button.enum.ts
@@ -0,0 +1,8 @@
+export enum Buttons {
+  ADDCOAST = 'Add coast',
+  ADDINCOME = 'Add income',
+  SHOWCOASTS = 'Show coasts',
+  SHOWINCOMES = 'Show incomes',
+  STATIST = 'Statistics',
+  GRAPHS = 'Graphs'
+}
