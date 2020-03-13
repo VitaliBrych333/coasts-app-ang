7145cd09cfd6b72aad6c36d8ce09719100f702ed
Vitali Brych <vbrych@cpaglobal.com>
2020-03-13 23:58:48 +0300

    feat: add tests for loading interceptor

---
+++ src/app/core/loading-screen/loading.iterceptor.spec.ts
@@ -0,0 +1,44 @@
+import { HttpHandler, HttpRequest } from '@angular/common/http';
+import { of } from 'rxjs/Observable/of';
+import { LoadingScreenService } from './loading-screen.service';
+import { LoadingScreenInterceptor } from './loading.interceptor';
+
+const expect = chai.expect;
+const moduleName = 'Core';
+const componentName = 'LoadingScreenInterceptor';
+
+describe(`${moduleName}.${componentName}`, () => {
+  let testTarget: LoadingScreenInterceptor;
+  let loadingScreenServiceMock: any;
+
+  beforeEach(() => {
+    loadingScreenServiceMock = sinon.createStubInstance(LoadingScreenService);
+    testTarget = new LoadingScreenInterceptor(loadingScreenServiceMock);
+  });
+
+  describe('#intercept', () => {
+    it('should set activeRequests in 1', () => {
+      // Arrange
+      const request = new HttpRequest('GET', 'test');
+      const next = { handle: sinon.stub().returns(of(['test'])) } as HttpHandler;
+
+      // Act
+      testTarget.intercept(request, next);
+
+      // Assert
+      expect(testTarget.activeRequests).to.eql(1);
+    });
+
+    it('should set activeRequests in 0', () => {
+      // Arrange
+      const request = new HttpRequest('GET', '/login');
+      const next = { handle: sinon.stub().returns(of(['test'])) } as HttpHandler;
+
+      // Act
+      testTarget.intercept(request, next);
+
+      // Assert
+      expect(testTarget.activeRequests).to.eql(0);
+    });
+  });
+});
