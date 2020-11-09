ee20850aed3980aa8b231f233b3e927e593d39e1
Vitali Brych <vbrych@cpaglobal.com>
2020-11-10 01:20:46 +0300

    refactor: update name

--- server/config/config.js
+++ server/config/config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
-  SECRET_CODE: '', // your secret code
+  SECRET_CODE: 'mySecretCode', // your secret code
   TIME_TOKEN: 3600, // token expiration time = 1 hour
-  URL_DB: '', // your url DB
+  URL_DB: 'mongodb://admin:admin1989@ds235417.mlab.com:35417/coasts?retryWrites=false', // your url DB
   PORT: 5500
 }
 
